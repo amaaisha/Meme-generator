4e2d9913c83a54f7b48280a04c0bd3c5e0ddbb05
Stanjhae <ceejayorji@yahoo.com>
2019-08-23 16:02:43 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header= () => {
+  return (
+    <header>
+      <img
+        src='https://lezebre.lu/images/detailed/71/17251-asian-troll-face-visage-asiatique.png'
+
+        alt={'Problem ?'}/>
+        <h1>MEME GENERATOR</h1>
+    </header>
+
+  )
+}
+
+
+
+
+
+export default Header

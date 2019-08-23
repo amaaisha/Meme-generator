4ab538da20cd688e338c89c4fb59646ad7598d2e
Administrator <suleamaaisha@:yahoo.com>
2019-08-23 16:28:43 +0200

    test3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MemeGenerator from "./components/MemeGenerator";
 function App() {
   return (
     <div>
-      <Header/>
+      <Header/>git config --global --unset credential.helper
       <MemeGenerator/>
     </div>
   )
